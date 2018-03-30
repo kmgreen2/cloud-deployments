a42d47bc1842e13608768768129d55ebc8c95e89
Kevin Greenan <kmgreen2@gmail.com>
2018-03-30 11:36:29 -0700

    Some changes to get ethereum running in AWS

--- service/ethereum/start_membernode.sh
+++ service/ethereum/start_membernode.sh
@@ -6,8 +6,8 @@ ETH_BOOT_NODE=""
 NUM_TRIES=0
 
 while [[ -z $ETH_BOOT_NODE ]]; do
-    ETH_BOOT_NODE=`curl http://ethereum-boot.ethereum.svc.cluster.local:80`
-    if (( NUM_TRIES > 3 )); then
+    ETH_BOOT_NODE=`curl --max-time 30 http://ethereum-boot.ethereum.svc.cluster.local:80`
+    if (( NUM_TRIES > 5 )); then
         echo "Could not connect to the bootstrap server: ethereum-boot.ethereum.svc.cluster.local:80"
         exit 1
     fi
@@ -16,10 +16,9 @@ done
 
 if [[ -n $ETH_DEBUG ]]; then
     DLV="dlv --headless=true --listen=:$ETH_DEBUG_PORT --log --api-version=2 exec"
-    CMD=$DLV" geth -- --datadir="$ETH_DATA_DIR" -verbosity 6 --ipcdisable --port $ETH_PORT --rpcport $ETH_RPC_PORT --networkid 1337 --bootnodes $ETH_BOOT_NODE > $ETH_LOG_DIR/membernode.log 2>&1"
-else
-    CMD="bootnode --verbosity 6 --nodekey=$ETH_DATA_DIR/boot.key > $ETH_LOG_DIR/bootnode.log 2>&1"
 fi
+
+CMD=$DLV" geth -- --datadir="$ETH_DATA_DIR" -verbosity 6 --ipcdisable --port $ETH_PORT --rpcport $ETH_RPC_PORT --networkid 1337 --bootnodes $ETH_BOOT_NODE > $ETH_LOG_DIR/membernode.log 2>&1"
 
 
 exec /bin/sh -c "$CMD"
