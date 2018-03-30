98fc89680d9250e551b56495234faf814ea0d4d7
Kevin Greenan <kmgreen2@gmail.com>
2018-03-30 06:31:30 -0700

    Updated the docker manifests to work with the new layout and updated the Kafka k8s manifest (it now runs in AWS)

--- scripts/docker/kafka/build.sh
+++ scripts/docker/kafka/build.sh
@@ -1,9 +1,9 @@
 BASE_DIR=`dirname $0`"/../../.."
 DOCKER_MANIFEST_DIR=${BASE_DIR}/manifests/docker/kafka
 
-docker build ${BASE_DIR} -f ${DOCKER_MANIFEST_DIR}/Dockerfile -t kmgreen2/kafka:0.10.2.1
+sudo docker build ${BASE_DIR} -f ${DOCKER_MANIFEST_DIR}/Dockerfile -t kmgreen2/kafka:0.10.2.1
 
 if [[ $1 == "push" ]]; then
-    docker push kmgreen2/kafka:0.10.2.1
+    sudo docker push kmgreen2/kafka:0.10.2.1
 fi
 
--- scripts/docker/zk/build.sh
+++ scripts/docker/zk/build.sh
@@ -1,9 +1,9 @@
 BASE_DIR=`dirname $0`"/../../.."
 DOCKER_MANIFEST_DIR=${BASE_DIR}/manifests/docker/zk
 
-docker build ${BASE_DIR} -f ${DOCKER_MANIFEST_DIR}/Dockerfile -t kmgreen2/zookeeper:3.4.11
+sudo docker build ${BASE_DIR} -f ${DOCKER_MANIFEST_DIR}/Dockerfile -t kmgreen2/zookeeper:3.4.11
 
 if [[ $1 == "push" ]]; then
-    docker push kmgreen2/zookeeper:3.4.11
+    sudo docker push kmgreen2/zookeeper:3.4.11
 fi
 
