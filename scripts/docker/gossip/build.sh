ec0ae7b55aec6ab2f6688ad4e217f12d0dcd2522
Kevin Greenan <kmgreen2@gmail.com>
2018-08-23 11:22:13 -0700

    Add docker and k8s manifests for Gossip

---
+++ scripts/docker/gossip/build.sh
@@ -0,0 +1,15 @@
+BASE_DIR=`dirname $0`"/../../.."
+DOCKER_MANIFEST_DIR=${BASE_DIR}/manifests/docker/gossip
+BINS="addpeer server createevent"
+
+for bin in ${BINS}; do
+    docker build --build-arg GITHUB_TOKEN=`cat ~/.gitaccess` ${BASE_DIR} -f ${DOCKER_MANIFEST_DIR}/Dockerfile.${bin} -t kmgreen2/gossip-${bin}:latest
+
+    if [[ $? != "0" ]]; then
+        break
+    fi
+
+    if [[ $1 == "push" ]]; then
+        sudo docker push kmgreen2/gossip-${bin}:latest
+    fi
+done
