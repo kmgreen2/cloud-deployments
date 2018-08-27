21493a94d095354c759de75e36676a5f21e29125
Kevin Greenan <kmgreen2@gmail.com>
2018-08-26 18:32:15 -0700

    Force build of server

--- scripts/docker/gossip/build.sh
+++ scripts/docker/gossip/build.sh
@@ -1,7 +1,6 @@
 BASE_DIR=`dirname $0`"/../../.."
 DOCKER_MANIFEST_DIR=${BASE_DIR}/manifests/docker/gossip
-#BINS="addpeer server createevent dumpevents"
-BINS="dumpevents"
+BINS="addpeer server createevent dumpevents"
 
 for bin in ${BINS}; do
     sudo docker build --build-arg GITHUB_TOKEN=`cat ~/.gitaccess` ${BASE_DIR} -f ${DOCKER_MANIFEST_DIR}/Dockerfile.${bin} -t kmgreen2/gossip-${bin}:latest
