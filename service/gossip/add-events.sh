e73336cea65f49885b4b8a1c453127cb52d09add
Kevin Greenan <kmgreen2@gmail.com>
2018-08-27 15:47:36 -0700

    Update add events script

--- service/gossip/add-events.sh
+++ service/gossip/add-events.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 if [[ -z $1  || -z $2 || -z $3 ]]; then
-    echo "Usage: $0 <master> <num events> <peer port>"
+    echo "Usage: $0 <master> <start timestamp> <num events> <peer port>"
     exit 1
 fi
 
 MASTER=$1
-let NUM_EVENTS=$2
-PORT=$3
+let START=$2
+let NUM_EVENTS=$3
+let NUM_EVENTS=$NUM_EVENTS+$2
+PORT=$4
 
 declare -a PEER_IPS=(`kubectl get pods -n gossip-server -o wide | grep 'gossip-server' | awk '{print $6;}'`)
 
-let i=0
+let i=${START}
 while (( i < NUM_EVENTS )); do
     peer=${PEER_IPS[$RANDOM % ${#PEER_IPS[@]}]}
     echo "Adding event $i to $peer"
-    ssh -i /root/.ssh/id_rsa_instance -o StrictHostKeyChecking=no admin@${MASTER} "sudo docker run kmgreen2/gossip-createevent /createevent -server ${peer}:${PORT} -event $i"
+    ssh -i /root/.ssh/id_rsa_instance -o StrictHostKeyChecking=no admin@${MASTER} "sudo docker run kmgreen2/gossip-createevent /createevent -server ${peer}:${PORT} -event evt-$i"
     let i=$i+1
 done
 
